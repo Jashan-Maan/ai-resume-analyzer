983d34f4ac2a2625eef74b644bd6b6725bac0fa0
Jashan Maan <jashanmaan521@gmail.com>
2026-05-26 13:29:07 +0530

    feat: implement MongoDB connection utility and configure environment variables

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+type Connection = {
+  isConnected?: number;
+};
+
+const connection: Connection = {};
+
+async function dbConnect(): Promise<void> {
+  if (connection.isConnected == 1) {
+    console.log("Already connected");
+    return;
+  }
+
+  if (!process.env.MONGODB_URI) {
+    throw new Error("MONGODB_URI is not defined in .env file");
+  }
+
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI);
+
+    connection.isConnected = db.connections[0].readyState;
+    console.log("Db Connection successful");
+  } catch (error: any) {
+    console.log("Db connection failed", error.message);
+    throw new Error("Failed to connect to database");
+  }
+}
+
+export default dbConnect;
