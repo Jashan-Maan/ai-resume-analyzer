d600d1c72ecd31990717afd15f28bc2e7f9f914a
Jashan Maan <jashanmaan521@gmail.com>
2026-06-12 15:09:47 +0530

    feat: .env naming fixes

--- src/services/email.ts
+++ src/services/email.ts
@@ -5,8 +5,8 @@ const OAuth2 = google.auth.OAuth2;
 
 async function createTransporter() {
   const oauth2Client = new OAuth2(
-    process.env.GMAIL_CLIENT_ID_GMAIL!,
-    process.env.GMAIL_CLIENT_SECRET_GMAIL!,
+    process.env.GMAIL_CLIENT_ID!,
+    process.env.GMAIL_CLIENT_SECRET!,
     "https://developers.google.com/oauthplayground",
   );
 
@@ -16,13 +16,17 @@ async function createTransporter() {
 
   const accessToken = await oauth2Client.getAccessToken();
 
+  if (!accessToken.token) {
+    throw new Error("Failed to get Gmail access token");
+  }
+
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
       type: "OAuth2",
       user: process.env.EMAIL_USER!,
-      clientId: process.env.GMAIL_CLIENT_ID_GMAIL!,
-      clientSecret: process.env.GMAIL_CLIENT_SECRET_GMAIL!,
+      clientId: process.env.GMAIL_CLIENT_ID!,
+      clientSecret: process.env.GMAIL_CLIENT_SECRET!,
       refreshToken: process.env.GMAIL_REFRESH_TOKEN!,
       accessToken: accessToken.token!,
     },
